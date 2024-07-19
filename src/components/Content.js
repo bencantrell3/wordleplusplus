1aefd5170e6cc09b66f9ac668a433f08a584b582
BEN CANTRELL <ben.cantrell@jbhunt.com>
2024-07-18 23:17:45 -0500

    back to old paths

--- src/components/Content.js
+++ src/components/Content.js
@@ -35,7 +35,7 @@ await fetchData();
 
 let validWords = [];
 function fetchAndLogTextFile() {
-  fetch('/allWords.txt')
+  fetch('https://bencantrell3.github.io/wordleplusplus/allWords.txt')
     .then(response => {
       if (!response.ok) {
         throw new Error(`ERROR FETCHING ALL WORDS ${response.status}`);
@@ -54,7 +54,7 @@ function fetchAndLogTextFile() {
 }
 let allAnswers = [];
 function fetchAndLogTextFile2() {
-  fetch('/allAnswers.txt')
+  fetch('https://bencantrell3.github.io/wordleplusplus/allAnswers.txt')
     .then(response => {
       if (!response.ok) {
         throw new Error(`ERROR FETCHING ALL ANSWERS ${response.status}`);
